a13ae3afbd5c3e67b2267714c7f4d8d4b8664a4f
FLDPMpang <frozenpang@gmail.com>
2021-10-24 03:11:24 +0800

    init

---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,11 @@
+// nav
+module.exports = [
+    { text: '首页', link: '/' },
+    { text: '索引', link: '/archives/',},
+    { text: '分类', link: '/categories/' },
+    { text: '标签', link: '/tags/' },
+    { text: '归档', link: '/archives/' },
+    { text: '关于', link: '/about/' },
+    { text: '友链', link: '/friends/'}
+  
+]
