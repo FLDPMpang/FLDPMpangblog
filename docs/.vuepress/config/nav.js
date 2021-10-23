4411db4348d73463be15c06899ee8fdf2158892c
FLDPMpang <frozenpang@gmail.com>
2021-10-24 03:45:36 +0800

    update readme

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,10 +1,9 @@
 // nav
 module.exports = [
     { text: '首页', link: '/' },
-    { text: '索引', link: '/archives/',},
+    { text: '归档', link: '/archives/' },
     { text: '分类', link: '/categories/' },
     { text: '标签', link: '/tags/' },
-    { text: '归档', link: '/archives/' },
     { text: '关于', link: '/about/' },
     { text: '友链', link: '/friends/'}
   
