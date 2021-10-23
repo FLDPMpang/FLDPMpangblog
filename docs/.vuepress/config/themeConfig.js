094673ab0e8af0b4c74286fda3725a10fd94147e
FLDPMpang <frozenpang@gmail.com>
2021-10-24 04:16:32 +0800

    add beian ,friends

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -86,7 +86,7 @@ module.exports = {
     // 页脚信息
     createYear: 2020, // 博客创建年份
     copyrightInfo:
-      '@2021FLDPMpang | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签
+      '@2021FLDPMpang | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a><br></br> <a href="https://beian.miit.gov.cn" target="_blank">鲁ICP备2021038338号-1</a>', // 博客版权信息，支持a标签
   },
   htmlModules // 插入hmtl(广告)模块
 }
